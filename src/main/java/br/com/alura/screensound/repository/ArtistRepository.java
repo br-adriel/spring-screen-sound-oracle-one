f822b941d874d1e161888e0d2de7378aeabe2572
Adriel <adriel.fsantos@outlook.com>
2024-05-07 17:48:58 -0300

    feat: Add findAllByNameContainingIgnoreCase method

--- src/main/java/br/com/alura/screensound/repository/ArtistRepository.java
+++ src/main/java/br/com/alura/screensound/repository/ArtistRepository.java
@@ -3,8 +3,11 @@ package br.com.alura.screensound.repository;
 import br.com.alura.screensound.models.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
     Optional<Artist> findByName(String name);
+
+    List<Artist> findAllByNameContainingIgnoreCase(String name);
 }
