0d53e4a78fb781ce38d37bbcb34dab0e07aec545
Adriel <adriel.fsantos@outlook.com>
2024-05-07 12:34:34 -0300

    feat: Add ArtistRepository

---
+++ src/main/java/br/com/alura/screensound/repository/ArtistRepository.java
@@ -0,0 +1,7 @@
+package br.com.alura.screensound.repository;
+
+import br.com.alura.screensound.models.Artist;
+import org.springframework.data.repository.Repository;
+
+public interface ArtistRepository extends Repository<Artist, Long> {
+}
