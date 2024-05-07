2eff53ffa837aeff2adb8f14a7b45d227373c59f
Adriel <adriel.fsantos@outlook.com>
2024-05-07 17:49:20 -0300

    feat: Add findAllByBirthDateGreaterThanEqualLessThanEqual method

--- src/main/java/br/com/alura/screensound/repository/ArtistRepository.java
+++ src/main/java/br/com/alura/screensound/repository/ArtistRepository.java
@@ -3,6 +3,7 @@ package br.com.alura.screensound.repository;
 import br.com.alura.screensound.models.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,4 +11,6 @@ public interface ArtistRepository extends JpaRepository<Artist, Long> {
     Optional<Artist> findByName(String name);
 
     List<Artist> findAllByNameContainingIgnoreCase(String name);
+
+    List<Artist> findAllByBirthDateGreaterThanEqualLessThanEqual(LocalDate minDate, LocalDate maxDate);
 }
