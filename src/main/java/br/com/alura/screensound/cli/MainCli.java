de0a1f2bd16f1677eeb963e772ecf1b62c40facf
Adriel <adriel.fsantos@outlook.com>
2024-05-07 14:09:37 -0300

    feat: Implement registerArtist cli method

--- src/main/java/br/com/alura/screensound/cli/MainCli.java
+++ src/main/java/br/com/alura/screensound/cli/MainCli.java
@@ -1,8 +1,12 @@
 package br.com.alura.screensound.cli;
 
+import br.com.alura.screensound.models.Artist;
+import br.com.alura.screensound.models.enums.ArtistType;
 import br.com.alura.screensound.repository.ArtistRepository;
 import br.com.alura.screensound.repository.SongRepository;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainCli {
@@ -57,6 +61,54 @@ public class MainCli {
     }
 
     private void registerArtist() {
+        var opcao = -1;
+        while (opcao != 0) {
+            System.out.println("\nCadastro de artista =====================");
+
+            var artist = new Artist();
+            System.out.print("Nome: ");
+            artist.setName(sc.nextLine());
+
+            System.out.print("Biografia: ");
+            artist.setBio(sc.nextLine());
+
+            System.out.print("Tipo (Solo, Dupla ou Banda): ");
+            artist.setType(ArtistType.fromPortugueseTranslation(sc.nextLine()));
+
+            System.out.print("Gêneros (separe come espaço e vírgula): ");
+            var genres = Arrays.asList(sc.nextLine().split(", "));
+            artist.setGenres(genres);
+
+            if (artist.getType() == ArtistType.SOLO) {
+                System.out.print("Data de nascimento: ");
+                artist.setBirthDate(LocalDate.parse(sc.nextLine()));
+            }
+
+            System.out.print("Website: ");
+            artist.setBio(sc.nextLine());
+
+            System.out.println("\n[i] - Salvando...");
+            artistRepository.save(artist);
+            System.out.println("[i] - Artista salvo!\n");
+
+            var validOption = false;
+            while (!validOption) {
+                System.out.println("Deseja cadastrar outro artista?");
+                System.out.println("0 - Não");
+                System.out.println("1 - Sim");
+                System.out.print(">> ");
+                opcao = sc.nextInt();
+
+                switch (opcao) {
+                    case 0:
+                    case 1:
+                        validOption = true;
+                        break;
+                    default:
+                        System.out.println("\n[!] - Opção inválida\n");
+                }
+            }
+        }
     }
 
     private void registerSong() {
